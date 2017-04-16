a090bf871daf2ef36cb40fbe15235938cbe685c2
Sergey Shimanskiy <flyer8@yandex.ru>
2017-04-16 22:18:20 +0300

    ...

--- smoke-test.sh
+++ smoke-test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Waiting 1 min for starting Tomcat with Java application"
+sleep 60
 echo "Verifying HTTP methods"
-curl --head --silent http://localhost:8080 | head -n1
-curl http://localhost:8080/message -X POST -d '{"messageId":1, "timestamp":1234, "protocolVersion":"1.0.0", "messageData":{"mMX":1234, "mPermGen":1234}}'
+echo "GET response is -" $(curl --head --silent http://localhost:8080 | head -n1)
+echo "POST request is -" $(curl http://localhost:8080/message -X POST -d '{"messageId":1, "timestamp":1234, "protocolVersion":"1.0.0", "messageData":{"mMX":1234, "mPermGen":1234}}') \
